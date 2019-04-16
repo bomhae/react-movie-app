162240a44ead0582150de99fdaf947ba1caf94ea
Bomhae <tlsgjs7764@gmail.com>
2019-04-16 15:26:47 +0900

    test) how to show 'loading--' before get data (with setTimeOut)

--- src/App.js
+++ src/App.js
@@ -5,22 +5,6 @@ import './App.css';
 class App extends Component {
   
   state = {
-    movies : [{
-      title : "matrix",
-      poster : "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
-    },
-    {
-      title : "Full Metal Jacket",
-      poster : "https://images-na.ssl-images-amazon.com/images/I/91sTE00%2Bm%2BL._SY606_.jpg",
-    },
-    {
-      title : "Oldboy",
-      poster : "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-    },
-    {
-      title : "Star Wars",
-      poster : "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
-    }]
   }
 
   componentWillMount(){
@@ -30,7 +14,22 @@ class App extends Component {
     setTimeout(() => {
       this.setState({
         movies : [
-          ...this.state.movies,
+          {
+            title : "matrix",
+            poster : "https://images-na.ssl-images-amazon.com/images/I/51EG732BV3L._SY445_.jpg",
+          },
+          {
+            title : "Full Metal Jacket",
+            poster : "https://images-na.ssl-images-amazon.com/images/I/91sTE00%2Bm%2BL._SY606_.jpg",
+          },
+          {
+            title : "Oldboy",
+            poster : "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+          },
+          {
+            title : "Star Wars",
+            poster : "https://images-na.ssl-images-amazon.com/images/I/81WjGytz7HL._SY445_.jpg"
+          },
           {
             title : "TrainsPotting",
             poster : "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Trainspotting_ver2.jpg/250px-Trainspotting_ver2.jpg"
@@ -40,12 +39,17 @@ class App extends Component {
     },5000)
   }
 
+  _renderMovies = () =>{
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie key={index} title={movie.title} poster={movie.poster}/>;
+    })
+    return movies;
+  }
+
   render() {
     return (
       <div className="App">
-      {this.state.movies.map((movie, index) => {
-        return <Movie key={index} title={movie.title} poster={movie.poster}/>;
-      })}
+        {this.state.movies ? this._renderMovies() : 'loading'}
       </div>
     );
   }
